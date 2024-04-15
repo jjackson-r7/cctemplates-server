bd0ccbb1ef9da69cac3b2a9a5a846e7d08107f55
jjackson-r7 <josh_jackson@rapid7.com>
2024-04-15 12:46:32 -0500

    fixed some required fields

--- scantemplate-parser/flask-scanparser.py
+++ scantemplate-parser/flask-scanparser.py
@@ -30,58 +30,60 @@ def upload():
         uploaded_file = request.files.get('file')  # Handle None if no file uploaded
         case_number = request.form.get('case_number')
 
-        if uploaded_file:
-            try:
-                # Save the uploaded file to disk
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-                uploaded_file.save(file_path)
+        if not uploaded_file or not case_number:
+            return "Please upload a file and provide a case number."
 
-                # Process uploaded file
-                general_output = process_general_section(file_path)
-                asset_output = asset_discovery(file_path)
-                service_output = service_discovery(file_path)
-                discovery_output = discovery_performance(file_path)
-                vulnerability_output = vulnerability_checks(file_path)
-                scan_assistant_output = scan_assistant(file_path)
-                file_search_output = file_search(file_path)
-                spam_relay_output = spam_relay(file_path)
-                database_output = database(file_path)
-                mail_output = mail_servers(file_path)
-                cvs_output = cvs_servers(file_path)
-                dhcp_output = dhcp_servers(file_path)
-                telnet_output = telnet_servers(file_path)
-                policy_output = policy_manager(file_path)
-                oracle_output = oracle_policy(file_path)
-                lotus_output = lotus(file_path)
-                windowsg_output = windows_group(file_path)
-                cif_smb_output = cif_smb_account(file_path)
-                as_400_output = as_400_policy(file_path)
-                unix_output = unix_policy(file_path)
+        try:
+            # Save the uploaded file to disk
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+            uploaded_file.save(file_path)
 
-                 # Generate log message
-                timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                log_message = f"Timestamp: {timestamp}, Case Number: {case_number}\n"
+            # Process uploaded file
+            general_output = process_general_section(file_path)
+            asset_output = asset_discovery(file_path)
+            service_output = service_discovery(file_path)
+            discovery_output = discovery_performance(file_path)
+            vulnerability_output = vulnerability_checks(file_path)
+            scan_assistant_output = scan_assistant(file_path)
+            file_search_output = file_search(file_path)
+            spam_relay_output = spam_relay(file_path)
+            database_output = database(file_path)
+            mail_output = mail_servers(file_path)
+            cvs_output = cvs_servers(file_path)
+            dhcp_output = dhcp_servers(file_path)
+            telnet_output = telnet_servers(file_path)
+            policy_output = policy_manager(file_path)
+            oracle_output = oracle_policy(file_path)
+            lotus_output = lotus(file_path)
+            windowsg_output = windows_group(file_path)
+            cif_smb_output = cif_smb_account(file_path)
+            as_400_output = as_400_policy(file_path)
+            unix_output = unix_policy(file_path)
 
-                # Save log to file
-                log_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'log.txt')
-                with open(log_file_path, 'a') as log_file:
-                    log_file.write(log_message)
-                    
-                # Combine results
-                combined_result = '\n\n'.join([
-                    general_output, asset_output, service_output, discovery_output,
-                    vulnerability_output, scan_assistant_output, file_search_output,
-                    spam_relay_output, database_output, mail_output, cvs_output,
-                    dhcp_output, telnet_output, policy_output, oracle_output,
-                    lotus_output, windowsg_output, cif_smb_output, as_400_output,
-                    unix_output
-                ])
+            # Generate log message
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            log_message = f"Timestamp: {timestamp}, Case Number: {case_number}\n"
 
-                # Remove uploaded file
-                os.remove(file_path)
+            # Save log to file
+            log_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'log.txt')
+            with open(log_file_path, 'a') as log_file:
+                log_file.write(log_message)
+                
+            # Combine results
+            combined_result = '\n\n'.join([
+                general_output, asset_output, service_output, discovery_output,
+                vulnerability_output, scan_assistant_output, file_search_output,
+                spam_relay_output, database_output, mail_output, cvs_output,
+                dhcp_output, telnet_output, policy_output, oracle_output,
+                lotus_output, windowsg_output, cif_smb_output, as_400_output,
+                unix_output
+            ])
 
-                return render_template("output.html", upload_result=combined_result)
-            except Exception as e:
+            # Remove uploaded file
+            os.remove(file_path)
+
+            return render_template("output.html", upload_result=combined_result)
+        except Exception as e:
                 return f"Error processing the uploaded file: {str(e)}"
         else:
             return "Please upload a file and provide a case number."
